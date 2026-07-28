53e6d51efdf42ba833cbd3fc0ea8b8214c4082eb
Rasul <nizomovrasul@gmail.com>
2026-07-28 12:48:29 +0300

    Rasul

---
+++ script.js
@@ -0,0 +1,22 @@
+<script>
+
+function showCars(){
+
+document.getElementById("cars")
+.scrollIntoView({
+behavior:"smooth"
+});
+
+}
+
+
+function contact(){
+
+document.getElementById("contact")
+.scrollIntoView({
+behavior:"smooth"
+});
+
+}
+
+</script>
